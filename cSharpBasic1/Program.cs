ff55e24ccb82e3a6d2ffdb02fae042948cb6021d
Dejan Kostenarov <87342103+ElderLearningDenz@users.noreply.github.com>
2022-03-08 20:31:07 +0100

    Add project files.

---
+++ cSharpBasic1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cSharpBasic1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Blah, Blah");
+            Console.WriteLine(23);
+            Console.WriteLine("Hello people of Blah blah land! \nThis is in new line");
+            /*Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");*/
+
+            /*Console.WriteLine("     *\n    ***\n   *****\n  *******\n *********");
+
+            Console.WriteLine("    *\n    *\n *  *  *\n*   *   *\n    *\n    *\n  *   *\n *     *");*/
+
+            Console.WriteLine("Please write some text...");
+            var inputText = Console.ReadLine();
+
+            Console.WriteLine(inputText);
+
+        }
+    }
+}
