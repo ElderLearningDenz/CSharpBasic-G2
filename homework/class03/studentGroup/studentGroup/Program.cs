26c529ad7cbbbd2dbc0ee3e194f960eadc3b678c
Dejan Kostenarov <dejan.kostenarov@gmail.com>
2022-03-17 15:26:52 +0100

    homework

---
+++ homework/class03/studentGroup/studentGroup/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace studentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentGroupOne = { "Kratos", "Aloy", "Drake", "Ellie", "Jin" };
+            string[] studentGroupTwo = { "Chief", "Markus", "Frank", "Senua", "Ori" };
+
+            
+
+            Console.WriteLine("Please enter a student group: (enter 1 or 2)");
+            string showStudents = Console.ReadLine();
+            int.TryParse(showStudents, out int printStudents);
+
+            switch (printStudents)
+
+            {
+                case 1:
+                    {
+                        foreach (string student in studentGroupOne)
+                        {
+                            Console.WriteLine(student);
+                        }
+                        break;
+
+                    }
+                case 2:
+                    {
+                        foreach (string student in studentGroupTwo)
+                        {
+                            Console.WriteLine(student);
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ homework/class03/sumOfEven/sumOfEven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace sumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sumOfEven = new int[6];
+
+            
+            for (int i = 0; i <6; i++)
+            {
+                Console.WriteLine($"Please Enter integer number {i+1}");
+                string number = Console.ReadLine();
+                int.TryParse(number, out int parsedNumber);
+                sumOfEven[i] = parsedNumber;
+                
+                
+            }
+
+            int sum = 0;
+            foreach (int num in sumOfEven)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+                
+            }
+            Console.WriteLine($"The sum of the array sumOfEven is: {sum}.");
+
+        }
+    }
+}
